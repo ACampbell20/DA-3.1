7dea86db626226a04da9569fe5a5d1253534aa9d
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-20 12:26:52 -0500

    Add Highlander class with constructors and destructor
    Implemented constructors and destructor for Highlander class, managing instance count.

--- Highlander.cpp
+++ Highlander.cpp
@@ -1,1 +1,27 @@
+#include "Highlander.h"
 
+int Highlander::count = 0;
+
+Highlander::Highlander(){
+name = "Unknown";
+count++;
+}
+
+Highlander::Highlander(string n) {
+name = n;
+count++; 
+}
+
+Highlander::~Highlander(){
+count--;
+}
+
+string Highlander::speak() {
+return "There can only be one. But now there are " + to_string(count);
+}
+
+string Highlander::status(){
+  if (count == 1)
+    return "Immortal";
+
+return "Murderous";
