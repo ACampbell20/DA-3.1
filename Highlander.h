862e5dd842098a999a10eca23ab3b4ee17dc9999
Ashanti Campbell <acampbell20@cub.uca.edu>
2026-07-20 12:23:48 -0500

    Correct syntax errors in Highlander.h
    Fixed include directive and class declaration syntax errors.

--- Highlander.h
+++ Highlander.h
@@ -1,1 +1,21 @@
+#ifndef HIGHLANDER_H
+#define HIGHLANDER_H
 
+#inlude <string>
+using namespace std;
+
+clas Highlander{
+  private:
+  string name;
+static int count;
+
+public:
+Highlander();
+Highlander(string n);
+~Highlander();
+
+static string speak();
+string status();
+};
+
+#endif
